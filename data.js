7e017b4ff3e5c5e07ee9225b01be35acec98e7eb
Merlu123 <merluciteste@yahoo.com>
2026-08-19 18:14:13 +0300

    Update cover path for 'Doamna Pylinska' entry

--- data.js
+++ data.js
@@ -161,7 +161,7 @@ const BOOKS = [
   {
     id:"doamnap",
     title:"Doamna Pylinska și secretul lui Chopin",
-    cover:"coperti/doamnap.png"
+    cover:"dictionarul-nedumerescu/coperti/doamnap.png"
   }
   
 ];
